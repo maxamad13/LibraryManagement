fbddeb4c255469532d9f4d03bae9389a95dac750
Sharif <sharifmo@vertafore.com>
2018-12-29 14:06:04 -0600

    add web layer and data layer on first data migration

--- Library/Library/Startup.cs
+++ Library/Library/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,7 +23,10 @@ namespace Library
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			//Add framwork services
             services.AddMvc();
+
+			services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Library/LibraryData/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+    public class Book : LibraryAsset
+    {
+		[Required] [Display(Name = "ISBN #")]
+		public string ISBN { get; set; }
+
+		[Required]
+		public string Author { get; set; }
+
+		[Required] [Display(Name = "DDC")]
+		public string DeweyIndex { get; set; }
+	}
+}
